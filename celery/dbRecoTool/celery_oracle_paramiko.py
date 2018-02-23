375e01977c01f0278417e3548badfdc0ba7a1b10
me <61566027@163.com>
2018-02-23 23:48:55 +0800

    f

--- celery/dbRecoTool/celery_oracle_paramiko.py
+++ celery/dbRecoTool/celery_oracle_paramiko.py
@@ -1,7 +1,4 @@
 # coding:utf-8
-'''
-celery worker -A celery_oracle_paramiko --loglevel=info -E -Q for_task_A
-'''
 from celery import Celery,platforms
 from utils import SSHConnected
 from utils import sql as oracle_sql
@@ -12,19 +9,19 @@ brokers = 'redis://10.70.61.97:6379/1'
 backend = 'redis://10.70.61.97:6379/2'
 
 app = Celery(broker=brokers, backend=backend)
-platforms.C_FORCE_ROOT = True
-class config1():
+platforms.C_FORCE_ROOT = True ##root 用户启动
+class config():
 	#BROKER_URL = "redis://10.70.61.97:6379/1"
 	#CELERY_RESULT_BACKEND = "redis://10.70.61.97:6379/2"
 
 	CELERY_QUEUES = (
         #Queue("default",Exchange("default"),routing_key="default"),
-	Queue("for_task_A",Exchange("for_task_A"),routing_key="task_a"),
+	Queue("run_sql",Exchange("run_sql"),routing_key="task_sql"), #当使用Redis作为broker时，Exchange的名字必须和Queue的名字一样
 	Queue("for_task_B",Exchange("for_task_B"),routing_key="task_a")
 	)
 
 	CELERY_ROUTES = {
-        'celery_oracle_paramiko.r_sql':{"queue":"for_task_A","routing_key":"task_a"},
+        'celery_oracle_paramiko.r_sql':{"queue":"run_sql","routing_key":"task_sql"},
         'tasks.taskB':{"queue":"for_task_B","routing_key":"task_b"}
 	}
         #task_default_queue= 'default'
@@ -43,7 +40,7 @@ class config1():
 	#CELERY_default_exchange_type = 'direct' # 默认的交换类型是direct 直接匹配   topic可以模糊匹配 
 	#CELERY_default_routing_key = 'default', # 默认的路由键是task.default，这个路由键符合上面的default队列
 
-c=config1()
+c=config()
 app.config_from_object(c)
 
 
