cb8e19c160b2c926e63d35a83698d9a12bb7c16f
taojun <61566027@163.com>
2018-03-02 18:00:15 +0800

    f

--- urllib_requests/django-api.py
+++ urllib_requests/django-api.py
@@ -6,11 +6,17 @@ def util(url):
     res = requests.get(url,headers=headers, auth=('admin','admin' ))
     return str(res.text)
 
-url = 'http://127.0.0.1:8000/users/'##定义http请求的地址，即1
+view_name='myapi'
+
+url = 'http://127.0.0.1:8000/%s/'%view_name ##定义http请求的地址，即1
+
+
 headers = {'Accept': "application/json"}##定义header头，用dict方式定义，即3
 
 res = requests.get(url,headers=headers, auth=('admin','admin' ))
 s=str(res.text)
+print s
+true='true'
+false='false'
 for i in eval(s):
-    print util(url=i.get('url'))
-
+    print util(url=url+str(i.get('id')))
