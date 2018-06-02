a67a2f64361b5613aedb3106eb3a84f16a3ad105
taojun <taojun_xt@teamsun.com.cn>
2018-06-02 11:33:15 +0800

    f

---
+++ urllib_requests/json_request.py
@@ -0,0 +1,31 @@
+#  -*- coding:utf-8 -*-
+# File http_post.py
+
+import urllib
+import urllib2
+import json
+
+
+def http_post():
+    url = 'http://172.16.134.123:8000/app_center/shipei'
+    values = {'data':[{'unitid': 1, 'vmid':[1,2]},{'unitid':2,'vmid':[1]}]}
+
+    # url = 'http://172.16.134.123:8000/app_center/Capsule_save'
+    # values = {"data": \
+    # [
+    # {"vmname": "vmdk-zabbix-3", "vmid": 1, \
+    # "product_list": \
+    # [{"product_name": "\u6570\u636e\u5e93", "product_id": 1}]}, \
+    # {"vmname": "rpmbuild-lixx", "vmid": 2, \
+    # "product_list": 
+    # [{"product_name": "web", "product_id":2}]}], \
+    # "task_name": "测试任务","task_tag":"测试任务标签","task_desc":"",\
+    # "app_id":1,"user_id":2}
+
+    jdata = json.dumps(values)             # 对数据进行JSON格式化编码
+    req = urllib2.Request(url, jdata)       # 生成页面请求的完整数据
+    response = urllib2.urlopen(req)       # 发送页面请求
+    return response.read()                    # 获取服务器返回的页面信息
+
+resp = http_post()
+print resp
