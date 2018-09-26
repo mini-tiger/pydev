32d84490bb08ac21e1c6886d22060888e9ae55c7
tj-thinkpad <61566027@163.com>
2018-09-26 09:43:13 +0800

    f

--- 二进制读取/11.py
+++ 二进制读取/11.py
@@ -10,3 +10,6 @@ with open(os.path.join(d,"bin_python_slice_file.txt"), "rb") as f:
 	# data = f.read(16)
 	# a = struct.unpack('<i', data)
 	# print(a)  # 将二进制数据转化为10进制数据。
+# todo 格式说明
+# https://blog.csdn.net/shudaqi2010/article/details/78133120
+# https://blog.csdn.net/zhongbeida_xue/article/details/79026333
